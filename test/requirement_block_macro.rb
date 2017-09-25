91b7e043d3d935a726ac6290e3c6f7b5499af6ef
bsmith-n4 <brian.smith@numberfour.eu>
2017-09-26 00:54:35 +0200

    add sample docs for requirement tests

--- test/requirement_block_macro.rb
+++ test/requirement_block_macro.rb
@@ -1,7 +1,6 @@
 require 'test/unit'
 require 'asciidoctor'
 require_relative '../lib/extensions/requirement_block_macro'
-require_relative '../lib/extensions/utils/requirement_walker'
 
 # Register the Requirement block macro 
 Extensions.register do
@@ -12,7 +11,13 @@ class TestRequirementBlock < Test::Unit::TestCase
 	def test_simple
 		input = "requirements::[]"
 
-	assert_equal("<h2 id=\"requirements\"><a class=\"anchor\" href=\"#requirements\"></a><a class=\"link\" href=\"#requirements\">Requirements</a></h2>\n    <div class=\"panel panel-default\"> <div class=\"panel-heading\"><h4>Requirements</h4></div>\n      <table class=\"table\"> <thead> <tr>\n        <th>#</th> <th>ID</th><th>Version</th> <th>Title</th> <th>Document</th>\n      </tr> </thead>\n      <tbody>\n        [req,id=123,version=1]samples/1Requirement Title\n\n      </tbody>\n      </table> </div>", 
+	assert_equal("<h2 id=\"requirements\"><a class=\"anchor\" href=\"#requirements\""\
+    "></a><a class=\"link\" href=\"#requirements\">Requirements</a></h2>\n    <div "\
+    "class=\"panel panel-default\"> <div class=\"panel-heading\"><h4>Requirements</h4>"\
+    "</div>\n      <table class=\"table\"> <thead> <tr>\n        <th>#</th> <th>ID</th>"\
+    "<th>Version</th> <th>Title</th> <th>Document</th>\n      </tr> </thead>\n      <tbody>\n"\
+    "        [req,id=123,version=1]samples/document1Requirement Title\n[req,id=456,version=1]"\
+    "samples/subdir/document2Requirement Title\n\n      </tbody>\n      </table> </div>", 
 		Asciidoctor::Document.new(input).render)
 	end
 end
