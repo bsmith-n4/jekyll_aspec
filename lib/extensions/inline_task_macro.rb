2f3ddb1652474108b2999ec39fb8fa8acbe043c6
bsmith-n4 <brian.smith@numberfour.eu>
2017-09-26 12:31:37 +0200

    Add basic docs, remove placeholder text

--- lib/extensions/inline_task_macro.rb
+++ lib/extensions/inline_task_macro.rb
@@ -9,11 +9,11 @@ Extensions.register do
     named :task
 
     process do |parent, target, attrs|
-      pattern =
-        (parent.document.attr 'task-pattern') ||
-        'https://jira.numberfour.eu/browse/%s'
+      pattern = parent.document.attr 'task-pattern'
       url = pattern % target
-      # Some utility functions used by similar inline macros
+      
+      # TODO add some smart handling of multiple target repos
+      
       label = Labels.getstatus(attrs)
       html = Context.form(attrs, target, url, label)
       (create_pass_block parent, html, attrs).render
