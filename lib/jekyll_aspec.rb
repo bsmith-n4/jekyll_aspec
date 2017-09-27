04ab22c2fac87a86df7abcf5c01dc9c21ea60634
bsmith-n4 <brian.smith@numberfour.eu>
2017-09-27 11:28:07 +0200

    minor fix

--- lib/jekyll_aspec.rb
+++ lib/jekyll_aspec.rb
@@ -6,6 +6,7 @@ require_relative "extensions/inline_repo_macro"
 require_relative "extensions/inline_task_macro"
 require_relative "extensions/req_preprocessor"
 require_relative "extensions/req_refs"
+require_relative "extensions/requirement_block"
 require_relative "extensions/requirement_block_macro"
 require_relative "extensions/todo_block"
 
