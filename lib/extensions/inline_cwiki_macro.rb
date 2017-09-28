f035271487d42729051e7a425c765508f4621bff
bsmith-n4 <brian.smith@numberfour.eu>
2017-09-28 16:07:50 +0200

    better inline task config, add tests

--- lib/extensions/inline_cwiki_macro.rb
+++ lib/extensions/inline_cwiki_macro.rb
@@ -14,18 +14,17 @@ Extensions.register do
 
     process do |parent, target, attrs|
       pattern = parent.document.attr 'cwiki-pattern'
+    
       if pattern.nil? 
         warn "asciidoctor: WARNING: Attribue 'cwiki-pattern' for inline repo macro not defined"
         pattern = "unknown"
         label = "warning"
-        url = ""
         target = "cwiki-pattern missing"
       else
-        url = "#{pattern}/#{target}"
         label = Labels.getstatus(attrs)        
       end
       
-      html = Context.format(attrs, target, url, label)
+      html = Context.format(attrs, target, pattern, label)
       (create_pass_block parent, html, attrs).render
     end
   end
--- test/inline_cwiki_macro.rb
+++ test/inline_cwiki_macro.rb
@@ -7,10 +7,18 @@ class TestInlineCwikiMacroProcessor < Test::Unit::TestCase
 	def test_missing_config
 		input = "cwiki:topic[]"
 
-  	assert_equal("<div class=\"paragraph\">\n<p><a href=\"\"><span"\
-    " class=\"label label-warning\">cwiki-pattern missing</span></a></p>\n</div>", 
+  	assert_equal("<div class=\"paragraph\">\n<p><span class=\"label label-warning\" data-toggle=\"tooltip\" title=\"Missing config\">cwiki-pattern missing</span></p>\n</div>", 
   		Asciidoctor::Document.new(input).render)
 	end
+
+  def test_simple_block
+    input = ":cwiki-pattern: http://www.example.org\ncwiki::topic[]"
+
+    assert_equal("<div class=\"paragraph\">\n<p><div style=\"float:"\
+      "right;padding-left:0.1em;\"><a href=\"http://www.example.org/"\
+      "topic\"><span class=\"label label-default\">topic</span></a></div></p>\n</div>", 
+      Asciidoctor::Document.new(input).render)
+  end
 
   def test_simple
     input = ":cwiki-pattern: http://www.example.org\ncwiki:topic[]"
@@ -20,7 +28,7 @@ class TestInlineCwikiMacroProcessor < Test::Unit::TestCase
       Asciidoctor::Document.new(input).render)
   end
 
-  def test_single_open_status
+  def test_single_open_label
     input = ":cwiki-pattern: http://www.example.org\ncwiki:topic[status=\"open\"]"
 
     assert_equal("<div class=\"paragraph\">\n<p><a href=\"http://www.example"\
@@ -28,7 +36,7 @@ class TestInlineCwikiMacroProcessor < Test::Unit::TestCase
       Asciidoctor::Document.new(input).render)
   end
 
-  def test_single_done_status
+  def test_single_done_label
     input = ":cwiki-pattern: http://www.example.org\ncwiki:topic[status=\"done\"]"
 
     assert_equal("<div class=\"paragraph\">\n<p><a href=\"http://www.example"\
